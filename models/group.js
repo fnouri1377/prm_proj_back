5202791eaa58c747762597b37e6c1565cfddc510
Fatemeh <nourifatemeh441@gmail.com>
2020-08-09 14:37:37 +0430

    last commit

--- models/group.js
+++ models/group.js
@@ -4,9 +4,6 @@ const groupSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: String,
     name: String,
-    // duration_message: Number, // chand rooz yek bar mikham be afrade in group message bedam?
-    // duration_call: Number, // chand rooz yek bar mikham be afrade in group zang bezanam?
-    // duration_meeting: Number, // chand rooz yek bar mikham afrade in group ro bebinam?
     note: String,
     contacts: [String]
 });
